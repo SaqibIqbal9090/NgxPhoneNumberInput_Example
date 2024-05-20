6af997c28f656ed751e6ae793e52bcf0d8bc23d9
Saqib Iqbal <123089457+SaqibIqbal9090@users.noreply.github.com>
2024-05-20 14:36:07 +0500

    demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+import { NgxPhoneNumberInputModule } from "ngx-lib-phone-input";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,NgxPhoneNumberInputModule,ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'form';
+
+  constructor(){ }
+number:any
+  MyForm = new FormGroup({
+    phoneNumber : new FormControl('')
+  })
+
+  submit(){
+    this.number =this.MyForm.value.phoneNumber || "";
+  }
 }
